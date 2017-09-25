602fe7f26eaf5f2b4a370f8034a300e27ab1d1cc
Neko4me <linyc47@163.com>
2017-09-25 13:47:44 +0800

    Main Blog app

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from blog.models import Topic,Post
+
+admin.site.register(Topic)
+admin.site.register(Post)
+
+# Register your models here.
---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post,Topic
+
+def index(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context={'posts': posts}
+    return render(request, 'blog/index.html', context)
+
+def topics(request):
+    topics=Topic.objects.order_by('date_added')
+    context={'topics': topics}
+    return render(request, 'blog/topics.html',context)
+
+def article(request,post_id):
+    post=Post.objects.get(id=post_id)
+    context={'post': post}
+    return render(request, 'blog/article.html',context)
