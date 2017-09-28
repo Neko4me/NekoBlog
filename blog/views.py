79914ced9f651e4d11b77fd895799f1ef47063b6
Neko4me <linyc47@163.com>
2017-09-28 12:42:38 +0800

    add Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post,Topic
+from django.core.paginator import  Paginator,EmptyPage,PageNotAnInteger
 
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    paginator=Paginator(posts,3)
+    page=request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator)
     context={'posts': posts}
     return render(request, 'blog/index.html', context)
 
@@ -13,7 +22,7 @@ def topics(request):
     return render(request, 'blog/topics.html',context)
 
 def article(request,post_id):
-    post=Post.objects.get(id=post_id)
+    post=get_object_or_404(Post,id=post_id)
     context={'post': post}
     return render(request, 'blog/article.html',context)
 
@@ -21,7 +30,7 @@ def about(request):
     return  render(request, 'blog/about.html')
 
 def topic(request,topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic,id=topic_id)
     posts=topic.post_set.order_by('-published_date')
     context={'topic':topic,'posts':posts}
     return render(request,'blog/topic.html',context)
